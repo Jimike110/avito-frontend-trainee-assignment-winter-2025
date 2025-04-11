af490986666950b4464ce9824be7d62bd9d9fc9a
Jimike110 <oladoyemike@gmail.com>
2025-04-12 01:10:22 +0300

    Created a test page for implementing test ui changes

---
+++ app/pages/Test/index.tsx
@@ -0,0 +1,13 @@
+import { Flex } from 'antd'
+import React from 'react'
+import './styles.css';
+
+const Test = () => {
+  return (
+    <Flex className='container' align='center' justify='center'>
+        Hello, world!
+    </Flex>
+  )
+}
+
+export default Test
